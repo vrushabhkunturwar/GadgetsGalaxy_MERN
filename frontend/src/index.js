32a988c91a36858f8a2c37cfb21c80a1b068c78e
vrushabhkunturwar <vrushabhkunturwar@gmail.com>
2024-04-17 10:32:50 +0530

    App Created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>
 );
 
